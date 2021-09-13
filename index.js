ec653fdf25ef082291bb8450b50bf4cf0b0a6756
Walter Morris <waltercmorris18@gmail.com>
2021-09-13 05:05:14 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Walter is the champion";
+document.body.append('h1');
+
+
